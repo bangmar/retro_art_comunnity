57e6434ae039f81430794d931dde065657768d43
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2022-12-06 16:44:37 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { GeneralProvider } from "./context/AppContext";
 import About from "./routes/About";
@@ -7,18 +7,20 @@ import Home from "./routes/Home";
 const App = () => {
 	return (
 		<React.Fragment>
-			<GeneralProvider>
-				<Routes>
-					<Route path='/' exact element={<About />} />
-					<Route
-						path='/home'
-						element={
-							<ProtectedRoute>
-								<Home />
-							</ProtectedRoute>
-						}></Route>
-				</Routes>
-			</GeneralProvider>
+			<HashRouter>
+				<GeneralProvider>
+					<Routes>
+						<Route path='/' exact element={<About />} />
+						<Route
+							path='/home'
+							element={
+								<ProtectedRoute>
+									<Home />
+								</ProtectedRoute>
+							}></Route>
+					</Routes>
+				</GeneralProvider>
+			</HashRouter>
 		</React.Fragment>
 	);
 };
